0b6a3ae06fb05dd9a139c0fecabdcc17d8357e70
rahulrjr <rahul.m.p@lxisoft.com>
2020-01-06 12:25:44 +0530

    ASCII code of a character

---
+++ Day1/acsii.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void main()
+{
+char ch;	
+printf("Enter a character:");
+scanf("%c",&ch);
+printf("ASCII code of %c is  %d " ,ch,ch);
+}
+
+	
+  
