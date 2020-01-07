4e44ceb54c4823215946aff5f5909d4c049ec230
rahulrjr <rahul.m.p@lxisoft.com>
2020-01-07 10:51:37 +0530

    Ascii code of a character

--- Day1/acsii.c
+++ Day1/acsii.c
@@ -1,45 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-void main() 
+void main()
 {
-	int i,j,k,a[10],lim,b[10],c[10];
-	printf("Enter limit:\t");
-	scanf("%d",&lim);
-	printf("Enter The numbers:\n");
-	
-	for(i=0;i<lim;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	
-	for(i=0;i<=lim;i++)
-	{
-		if(a[i]%2 == 0)
-		{	
-			
-			b[j] = a[i];
-			j++;
-		}
-		else
-		{
-			c[k] = a[i];
-			k++;
-		}
-		
-	}
-	
-	printf("Even Numbers:\n");
-	for(i=0;i<j;i++)
-	{
-		printf("%d \n",b[i]);
-	}
-	printf("Odd Numbers:\n");
-	for(i=0;i<k;i++)
-	{
-		printf("%d \n",c[i]);
-	}
+char ch;	
+printf("Enter a character:");
+scanf("%c",&ch);
+printf("ASCII code of %c is  %d " ,ch,ch);
 }
 
+	
+  
