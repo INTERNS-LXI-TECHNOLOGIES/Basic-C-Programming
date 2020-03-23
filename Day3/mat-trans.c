fef48a88c6b2f97780948cb69456065f22e2b265
Akhilesh-Narayanan <akhileshnarayanan.m.p@lxisoft.com>
2020-03-23 15:13:01 +0530

    http://jira.bytatech.com:8080/browse/IN-7860

---
+++ Day3/mat-trans.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	int a[10][10],b[10][10],i,j,x,y;
+	printf("Enter the order of matrices \n");
+	scanf("%d%d",&x,&y);
+	printf("\n Enter the first matrix\n");
+	for(i=0;i<x;i++)
+	{
+		for(j=0;j<y;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("\n Transpose of given matrix =>\n");
+	for(i=0;i<y;i++)
+	{
+		for(j=0;j<x;j++)
+		{
+			printf(" %d ",a[j][i]);
+		}
+		printf("\n");
+	}
+	
+}
