7cd0ed7c691a37a43f94847be28d98bdda6e2da5
aswathy30 <aswathy.m@lxisoft.com>
2020-03-23 23:29:24 +0530

    http://jira.bytatech.com:8080/browse/IN-7903

---
+++ Day2/pattern5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int row,column;
+	for(row=1;row<=5;row++)
+	{
+		for(column=1;column<=row;column++)
+		{
+			printf("%d",column);
+		}
+		printf("\n");
+	}
+}
