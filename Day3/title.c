03c8b170077ca7e62cd9ffa295846de588879d86
Akhilesh-Narayanan <akhileshnarayanan.m.p@lxisoft.com>
2020-03-23 13:43:29 +0530

    http://jira.bytatech.com:8080/browse/IN-7848

---
+++ Day3/title.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char s[30];
+	int i;
+	printf("Enter the string\n");
+	gets(s);
+	if(s[0]>='a'&&s[0]<='z')
+			{
+				s[0]=s[0]-32;
+			}
+	for(i=0;s[i]!='\0';i++)
+	{
+		if(s[i]==' ')
+		{
+			if(s[i+1]>='a'&&s[i+1]<='z')
+			{
+				s[i+1]=s[i+1]-32;
+			}
+		}
+	}
+	printf("title case => %s",s);
+}
