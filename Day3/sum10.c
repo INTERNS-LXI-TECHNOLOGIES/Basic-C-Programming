dfcb683901dd5909d8714bc7139ab48eb71f59fb
aswathy30 <aswathy.m@lxisoft.com>
2020-03-25 02:31:05 +0530

    http://jira.bytatech.com:8080/browse/IN-7974

---
+++ Day3/sum10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[10],i,sum=0;
+	printf("enter the elements");
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	sum=sum+i;
+	printf("the sum of ten numbers are %d",sum);
+}
