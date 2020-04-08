85d0a7975cb47f27b6e4cc0e792a844af65a98b9
faris-backer <faris.backer@lxisoft.com>
2020-04-08 14:26:52 +0530

    http://jira.bytatech.com:8080/browse/IN-8801

---
+++ Day2/d2p2_for_loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main ()
+{
+	int i,n,count=6;
+
+	Printf("\n Enter the number from which odd number shows",n);
+	scanf("%d",&n)
+
+	for(count=6,i=7; count>=n; i=i+2, count++);
+	{
+	printf("\n Results are %d",n);
+	
+	}
+	
+}
---
+++ Day2/d2p2_while_loop.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+void main()
+{
+	int n,count=6;
+	
+	Printf("\n Enter the number from which odd number shows",n);
+	scanf("%d",&n);
+	
+	
+	while(count>=n);
+	{
+		if(count!= 6)
+		printf("ODD numbers are %d ",count);	
+		count++;
+	}
+	
+	printf("Odd Numbers from 6 to %d:\n",n);
+}
