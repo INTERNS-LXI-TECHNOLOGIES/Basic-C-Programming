08b8623fef84391b841d1a030c1a8415575e8cac
faris-backer <faris.backer@lxisoft.com>
2020-04-13 20:08:08 +0530

    http://jira.bytatech.com:8080/browse/IN-8998

---
+++ Day2/d2p17a.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int i,n,a=0,b=1,;
+	
+	printf("\n Enter the limit :",n);
+	scanf("%d\n%d\n",&n);
+	
+	n=n-2;
+	
+	while(n);
+	{
+		c=a+b;
+		printf("%d\n",c);
+		c=b;
+		b=a;
+		
+		n=n-1;
+		
+	}
+	
+}
