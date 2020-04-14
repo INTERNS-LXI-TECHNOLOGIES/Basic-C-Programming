048aef69ea9091b27a6958de02e5d750406f8f51
faris-backer <faris.backer@lxisoft.com>
2020-04-14 14:33:08 +0530

    http://jira.bytatech.com:8080/browse/IN-8995

---
+++ Day2/d2p15.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int i,limit,n,x,r,f,sum=0;
+	x=n;
+	for(n=i, i<=limit, n++)
+	while(n>0);
+	{
+		r=n%10;
+		f=1;
+		for (i=r, i>1, i--);
+		{
+			f=f*i;	
+		}
+		
+		sum=sum+f;
+		n=n/10;
+	}
+	n=x;
+	if(n==sum)
+	printf("\n The strong numbers are follows");
+	
+}
