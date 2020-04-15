f281a279e8d27486c466aedcbfa3615ff087fd93
faris-backer <faris.backer@lxisoft.com>
2020-04-15 12:52:59 +0530

    http://jira.bytatech.com:8080/browse/IN-9057

---
+++ Day2/pattern3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int i,n,count;
+	
+	printf("\n Enter the value",n);
+	scanf("%d",&n);
+	
+	for(i=1; i<=n, i++)
+	{
+		int c=0
+		for (count=1; count<=i; count++)
+		{
+			if(i%count==0)
+		}
+	}	
+
+
+
+}
