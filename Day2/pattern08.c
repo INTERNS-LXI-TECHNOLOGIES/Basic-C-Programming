2dbab3a4ae4a4757a7c779dab617a16c34b49bce
faris-backer <faris.backer@lxisoft.com>
2020-04-15 15:22:35 +0530

    http://jira.bytatech.com:8080/browse/IN-9067

---
+++ Day2/pattern08.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int row,column;
+	for(column=1;column<=5;column++)
+	{
+		for(row=column;row<=5;row++)
+		{
+			printf("%d",row);	
+		}
+		printf("\n");
+		
+	}
+}
