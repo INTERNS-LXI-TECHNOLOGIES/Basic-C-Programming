f482b9efde3459c5f25a9e515aeb86f0991eb759
faris-backer <faris.backer@lxisoft.com>
2020-04-20 20:02:29 +0530

    http://jira.bytatech.com:8080/browse/IN-9308

---
+++ Day4/p6.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+void convert(int i) ;
+
+int main()
+{
+    int in ;
+
+    printf(" \n Input Measurement in Inches : ") ;
+    scanf("%d",&in) ;
+
+    convert(in) ;
+
+}
+
+void convert(int i)
+{
+    int f,in ;
+
+    f = i/12 ;
+    in = i%12 ;
+
+    printf("\n \n %d inches = %d feet and %d inches.",i,f,in) ;
+}
+
