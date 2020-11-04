70d6732a8cc738660d979ed589168db451697e3b
Jithin <jithin.p@lxisoft.com>
2020-11-04 09:41:39 +0530

    http://jira.bytatech.com:8080/browse/IN-11455

--- Day5/time.c
+++ Day5/time.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
+/*4.  Program to pass Time structure as argument and display the new Time.
+         Structure Time has three members hours, minutes, seconds.*/
 
+#include <stdio.h>
 struct  Time 
 {
     int hr, min, sec, day;
 }t0;
 
 struct Time t_time ( struct Time, struct  Time, struct  Time );
-void display ( struct  Time );
+void display ( struct Time, struct  Time, struct  Time );
 void main()
 {
     struct Time t1;
     struct Time t2;
     struct Time t3;
+    struct Time t0;
 
     printf("\n\nEnter 3 times");
 
@@ -40,12 +43,28 @@ void main()
     printf("Secs    : ");
     scanf("%d",&t3.sec);
 
-    display( t1, t2, t3, t0 );
+    display( t1, t2, t3 );
+}
+
+
+void display  ( struct Time t1, struct  Time t2, struct  Time t3 )
+{
     
+    printf("\n--------------\n");
+    printf("\n Data entered \n\n");
+
+    printf("\t  %d Hours : %d Mins : %d Seconds +\n", t1.hr, t1.min, t1.sec);
+    printf("\t  %d Hours : %d Mins : %d Seconds +\n", t2.hr, t2.min, t2.sec);
+    printf("\t  %d Hours : %d Mins : %d Seconds \n", t3.hr, t3.min, t3.sec);
+
+    printf("\nTotal time \n");
+
+    t_time ( t1, t2, t3 );
+
     printf("\n %d Days + %d Hours : %d Mins : %d Seconds ", t0.day, t0.hr, t0.min, t0.sec);
     printf("\n-------------------------------------------\n\n");
+}    
 
-}
 struct Time t_time ( struct  Time t1, struct  Time t2, struct  Time t3 )
 {
     t0.sec = t1.sec + t2.sec + t3.sec;
@@ -69,23 +88,4 @@ struct Time t_time ( struct  Time t1, struct  Time t2, struct  Time t3 )
         t0.day += 1;
         t0.hr -= 24;
     }
-    // printf("\n %d Days + %d Hours : %d Mins : %d Seconds ", t0.day, t0.hr, t0.min, t0.sec);
-    // printf("\n-------------------------------------------\n\n");
-    return t0;
 }
-
-void display  ( struct  Time t )
-    printf("\n--------------\n");
-    printf("\n Data entered \n\n");
-
-    printf("\t  %d Hours : %d Mins : %d Seconds +\n", t1.hr, t1.min, t1.sec);
-    printf("\t  %d Hours : %d Mins : %d Seconds +\n", t2.hr, t2.min, t2.sec);
-    printf("\t  %d Hours : %d Mins : %d Seconds \n", t3.hr, t3.min, t3.sec);
-
-    printf("\nTotal time \n");
-
-    t_time ( t1, t2, t3 );
-
-    printf("\n %d Days + %d Hours : %d Mins : %d Seconds ", t0.day, t0.hr, t0.min, t0.sec);
-    printf("\n-------------------------------------------\n\n");
-}    
