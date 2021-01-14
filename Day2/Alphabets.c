d26ba2d741836bf2548932dc22870a54b5d9badd
@ROHITH-77 <rohith.r@lxisoft.com>
2021-01-14 17:26:14 +0530

    http://jira.bytatech.com:8080/browse/IN-13044

---
+++ Day2/Alphabets.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char ch;
+    printf("Alphabets from a - z are: \n");
+    for(ch='a';ch<='z';ch++)
+    {
+        printf("%c\n", ch);
+    }
+    return 0;
+}
