b3452c2bfe04bfff52a16022f868f65194fda9a1
@ROHITH-77 <rohith.r@lxisoft.com>
2021-01-15 17:50:40 +0530

    http://jira.bytatech.com:8080/browse/IN-13074

---
+++ Day2/primebw2nos.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j,n,prime,a,b;
+	printf("Enter the intervel a & b : ");
+	scanf("%d%d",&a,&b);
+	i=a;
+	while(i<=b)
+	{
+		for(j=2;j<=(i/2);j++)
+		{
+			if(i%j==0)
+			{
+				prime=0;
+				break;
+			}
+			else
+			{
+				prime=1;
+			}
+		}
+		if(prime==1)
+		{
+			printf("\n %d",i);
+		}
+		i++;
+	}
+    
+}
