c4a84d5c61bf8ff8d15b38846f598eb6252ff36d
@ROHITH-77 <rohith.r@lxisoft.com>
2021-01-18 17:40:41 +0530

    http://jira.bytatech.com:8080/browse/IN-13104

---
+++ Day2/dob.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+    char ch;
+    int ly =0, lm = 0, ld = 0;
+    do
+    {   
+        char name[10];
+        int i,tempy, tempm, tempd, count = 0, x=100,d,m,y;
+        printf("\n Enter The name : ");
+        scanf("%c",&name);
+
+        printf("\nEnter date (dd/mm/yy) : ");
+        scanf("%d %d %d", &tempd,&tempm,&tempy);
+
+        ch=getchar();
+        if(ch=='~')
+        {
+            printf("\n\nYoungest Birthdate is %d / %d / %d\n\n",d,m,y);
+            break;
+        }
+
+        if (tempd > 0 && tempm > 0 && tempy > 0 ) 
+        {
+            for (i = 1; i < x ; i++) 
+            {
+                if ( tempy > ly )
+                {
+                    ly = tempy;
+                    y = ly;
+                    m = tempm;
+                    d = tempd;
+
+                    if ( tempy = ly )
+                    {   
+                        if ( tempm > lm )
+                        {
+                            lm = tempm;
+                            m = lm;
+                            d = tempd;
+                        }
+                                 
+                        if ( tempm = lm )
+                        {
+                            if ( tempd >= ld )
+                            {
+                                ld = tempd;
+                             d = ld;
+                            }  
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            printf("Invalid Input...!");
+        }
+    } 
+    while(1);        
+}
