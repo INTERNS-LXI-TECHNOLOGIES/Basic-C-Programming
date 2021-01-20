0dfc6d67e2584c29f3fdf117134bfad1893c60b9
@ROHITH-77 <rohith.r@lxisoft.com>
2021-01-20 17:44:32 +0530

    http://jira.bytatech.com:8080/browse/IN-13152

---
+++ Day3/sample1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+
+{
+    
+int i;
+
+    int a[5]={23,25,58,97,77};
+
+    float b[5]={4.5,6.7,2.6,7};
+
+    char name[10]={"raju"};
+ 
+   for(i=0;i<5;i++)
+
+    {
+ 
+       printf("\t %d",a[i]);
+
+        
+    }
+
+return 0;
+
+}
