dff2a6fc8d17a0c24220db1171c600bd84197fc1
@ROHITH-77 <rohith.r@lxisoft.com>
2021-01-22 17:38:56 +0530

    http://jira.bytatech.com:8080/browse/IN-13196

---
+++ Day3/summatrix.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[10][10],b[10][10],i,j,x,y;
+	printf("Enter the order of matrices \n");
+	scanf("%d%d",&x,&y);
+	printf("\n Enter the first matrix\n");
+	for(i=0;i<x;i++)
+	{
+		for(j=0;j<y;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("\n Enter the Second matrix\n");
+	for(i=0;i<x;i++)
+	{
+		for(j=0;j<y;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	
+	
+	
+	printf("\n sum of matrix =>\n");
+	for(i=0;i<x;i++)
+	{
+		for(j=0;j<y;j++)
+		{
+			printf(" %d ",a[i][j]+b[i][j]);
+		}
+		printf("\n");
+	}
+}
