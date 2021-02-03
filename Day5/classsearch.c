54ade414f3a077104c0a0cbfd4f237876b71cfb8
@ROHITH-77 <rohith.r@lxisoft.com>
2021-02-03 17:40:04 +0530

    http://jira.bytatech.com:8080/browse/IN-13460

---
+++ Day5/classsearch.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+struct student
+ 
+{
+
+char name[10];int rollno,classno,mark1,mark2,mark3;
+
+};
+
+void main ()
+
+{
+
+struct student st[5];
+
+int i;
+
+for(i=0;i<5;i++)
+
+{
+
+printf("Enter the Student Name :");
+
+scanf("%s",&st[i].name);
+
+printf("Enter Roll no :");
+
+scanf("%d",&st[i].rollno);
+
+printf("Enter Class no:");
+ 
+scanf("%d",&st[i].classno);
+
+printf("Enter Mark 1:");
+
+scanf("%d",&st[i].mark1);
+
+printf("Enter Mark 2:");
+
+scanf("%d",&st[i].mark2);
+
+printf("Enter Mark 3:");
+
+scanf("%d",&st[i].mark3);
+
+}
+
+printf("Your Input details are given below\n");
+
+for(i=0;i<5;i++)
+
+{
+ 
+   printf("\n Enter Classno to be search");
+
+scanf("%d",&classno);
+
+for(i=0;i<2;i++)
+
+{
+ 
+if(classno==st[i].classno)
+
+ {
+ 
+ printf("\n Name %s",st[i].name);
+
+  printf("\n Rollno %d",st[i].rollno);
+  
+printf("\n Classno %d",st[i].classno);
+
+ }
+
+printf("\nName :%s",st[i].name);
+
+printf("\nRoll No :%d",st[i].rollno);
+
+printf("\nClass No :%d",st[i].classno);
+
+printf("\nMark 1 :%s",st[i].mark1);
+
+printf("\nMark 1 :%s",st[i].mark2);
+
+printf("\nMark 1 :%s",st[i].mark3);
+
+}
+
+}
