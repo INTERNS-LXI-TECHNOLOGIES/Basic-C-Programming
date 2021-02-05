e21b0b995d83a0ddcbdf05ed1107c34bda673951
@ROHITH-77 <rohith.r@lxisoft.com>
2021-02-05 09:58:57 +0530

    http://jira.bytatech.com:8080/browse/IN-13429

---
+++ Day5/objectclass4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+struct student 
+{
+char name[10];char teach[10];int rollno,classno;
+
+};
+
+void main ()
+
+{
+
+struct student t1,t2;
+
+printf ("Name :");
+
+scanf("%s",&t1);
+
+printf ("Teacher :");
+
+scanf("%s",&t1);
+
+printf ("Rollno");
+
+scanf("%d",&t1);
+
+printf ("Class no :");
+
+scanf("%d",&t1);
+
+printf ("Name :");
+
+scanf("%s",&t2);
+
+printf ("Teacher :");
+
+scanf("%s",&t2);
+
+printf ("Rollno :");
+
+scanf("%d",&t2);
+
+printf ("Class no :");
+
+scanf("%d",&t2);
+
+}
