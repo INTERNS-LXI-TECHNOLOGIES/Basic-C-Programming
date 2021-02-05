3a243974471d4a165b7fcf835dff153e152565e3
@ROHITH-77 <rohith.r@lxisoft.com>
2021-02-05 10:00:29 +0530

    http://jira.bytatech.com:8080/browse/IN-13430

---
+++ Day5/emp.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+struct employee
+ 
+{
+
+char name[10];char desig[10];int sal,work_time;
+
+};
+
+void main ()
+
+{
+
+struct employee t1,t2;
+
+printf ("Name :");
+
+scanf("%s",&t1);
+
+printf ("Designation :");
+
+scanf("%s",&t1);
+
+printf ("Salary");
+
+scanf("%d",&t1);
+
+printf ("Work Time :");
+
+scanf("%d",&t1);
+
+printf ("Name :");
+
+scanf("%s",&t2);
+
+printf ("Designation :");
+
+scanf("%s",&t2);
+
+printf ("Salary :");
+
+scanf("%d",&t2);
+
+printf ("Work Time :");
+
+scanf("%d",&t2);
+
+}
