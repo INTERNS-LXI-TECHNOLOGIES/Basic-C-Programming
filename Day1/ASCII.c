5d96a89a9c34d3469b3cb8383c68dd1086e05c19
anjuzacharias <anju.zacharias@lxisoft.com>
2021-02-24 17:02:25 +0530

    http://jira.bytatech.com:8080/browse/IN-13851

---
+++ Day1/ASCII.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <math.h>
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf(" enter your character or string :");
+    scanf("%c",&a);
+    printf("ASCII value for %c is %d",a,a);
+   return 0;
+   
+}
