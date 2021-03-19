f3bdaaef0b81aa42b42a68ee3179ab6a8c5b1ef7
mahesan711 <mahesan.a.u@lxisoft.com>
2021-03-19 21:37:14 +0530

    http://jira.bytatech.com:8080/browse/IN-14288

---
+++ Day2/day2q2a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+int main()
+{
+	int i=7;
+	while(i<=199){
+	
+		printf("%d\n",i);
+		i=i+2;
+		}
+		getch();
+		}
---
+++ Day2/day2q2b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+int main()
+{
+	int i;
+	for(i=7;i<=199;i++)
+	{
+
+		printf("%d\n",i);
+	//	i=i+2;
+		}
+		getch();
+		}
