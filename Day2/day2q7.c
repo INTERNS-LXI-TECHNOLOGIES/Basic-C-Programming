2228ee8356e50f9fe2542020e4963de6572ab6ce
mahesan711 <mahesan.a.u@lxisoft.com>
2021-03-22 21:51:18 +0530

    #include <stdio.h>

---
+++ Day2/day2q7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+	int x,l;
+	a:
+	printf("Enter a number :");
+	scanf("%d",&l);
+	//printf("Enter the  number to end :");
+	//scanf("%d",&u);
+		//for(x=l;x<=u;x++)
+	//{
+		for(x=2;x<=l-1;x++)
+		
+		if(l%x==0)
+			break;
+			
+		if(x==l)
+			printf("%d is a prime number\n",l);	
+			else
+			printf("%d is not a  prime number\n",l);
+		
+		goto a;	
+			
+		
+		
+		
+	}
+	
+	
+	
+
---
+++ Day2/day2q8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,x;
+	printf("Enter first numbers to find LCM\n");
+	scanf("%d",&a);
+	printf("Enter second numbers to find LCM\n");
+	scanf("%d",&b);
+	
+	for(x=1;x<=a*b;x++)
+	if(x%a==0 && x%b==0)
+	break;
+	
+	
+	printf("LCM is %d",x);
+	}
