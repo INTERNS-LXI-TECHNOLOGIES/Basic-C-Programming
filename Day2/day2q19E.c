003e6e35a55827e62dad28d0cd2ab8d9be710211
mahesan711 <mahesan.a.u@lxisoft.com>
2021-03-29 20:32:02 +0530

    http://jira.bytatech.com:8080/browse/IN-14502

---
+++ Day2/day2q19E.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,k=0;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++){
+		 k++;
+		printf("%d",k);		
+			}
+		
+		
+		//for(k=1;k<=i;k++)
+		
+		//	printf("*");
+		
+	//	for(k=2;k<=i;k++)
+		
+		//	printf("*");
+		
+		
+		printf("\n");
+			
+	}
+	}
+
