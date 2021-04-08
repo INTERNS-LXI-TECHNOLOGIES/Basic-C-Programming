53bf37d5c0b38afcddc4ef23f9599a3aa1715683
mahesan711 <mahesan.a.u@lxisoft.com>
2021-04-08 22:10:04 +0530

    http://jira.bytatech.com:8080/browse/IN-14733

--- Day3/day3q12.c
+++ Day3/day3q12.c
@@ -1,24 +1,4 @@
-#include <stdio.h>
-int main()
-{
-	int i,j,A[5][5],B[5][5],sum=0,r,c;
-	printf("Enter Row and Coloms\n");
-	scanf("%d%d",&r,&c);
-	for(i=0;i<r;i++){
-	for(j=0;j<c;j++){
-	printf("enter element in %d,%d:",i+1,j+1);
-	scanf("%d",&A[i][j]);}	
-	}
-	printf("The matrix is\n ");
-	for(i=0;i<r;i++){
-	for(j=0;j<c;j++){
-	printf("%d\t",A[i][j]);
-	}
-	printf("\n");
-	}
-	
-	for(i=0;i<r;i++){
-		if(i==0){
+
 #include <stdio.h>
 int main(){
 	int r,c;
