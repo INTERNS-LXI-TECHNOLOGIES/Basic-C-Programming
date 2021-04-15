58fe05ea097d93dda77305ece73494589af37e16
mahesan711 <mahesan.a.u@lxisoft.com>
2021-04-15 22:24:38 +0530

    http://jira.bytatech.com:8080/browse/IN-14865

---
+++ Day4/day4q4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int string(char[100]);
+int main() {
+    char s[100];
+    int c;
+    printf("Enter the string\n");
+    gets(s);
+    c=string(s);
+       printf("Length of the string: %d", c);
+    
+    }
+    int string(char a[100]){
+int i;
+    
+    for(i=0; a[i]!='\0';i++){
+    
+ 
+    	}
+    return i;
+}
